e72d1b4f95131b8c8c679c5eb8bd6253998d9560
diejuse <45111636+diejuse@users.noreply.github.com>
2021-06-04 02:20:01 +0200

    Add files via upload

--- chrootMaemo.sh
+++ chrootMaemo.sh
@@ -6,6 +6,7 @@ export PULSE_SERVER=tcp:127.0.0.1:4713
 export CLUTTER_BACKEND=x11 HD_NOTHREADS=yes CLUTTER_DRIVER=gles2 OVERRIDE=-GL_EXT_unpack_subimage
 export USER=root
 export HOME=/root
+export XDG_RUNTIME_DIR=/run/user/0
 #export LANGUAGE=C
 #export LANG=C
 mount -o remount,exec,dev,suid $SDCARD
@@ -14,7 +15,7 @@ busybox mount --bind /proc $ROOT/proc
 busybox mount -t devpts none $ROOT/dev/pts
 busybox mount --bind /sdcard $ROOT/sdcard
 echo "Mounting Done"
-DISPLAY=:0     CLUTTER_BACKEND=x11 HD_NOTHREADS=yes CLUTTER_DRIVER=gles2 OVERRIDE=-GL_EXT_unpack_subimage busybox   chroot $ROOT /bin/bash -l
+busybox chroot $ROOT /bin/bash -l
 busybox umount $ROOT/dev/pts
 busybox umount $ROOT/dev
 busybox umount $ROOT/proc
